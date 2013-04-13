ce5c060025096366a7660ba378026624b8d433de
Raju Gandhi <raju.gandhi+github@gmail.com>
2013-04-13 10:04:38 -0400

    Put todos on $scope, and display them

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var myApp = angular.module("myApp", []);
+
+var firstCtrl = myApp.controller('FirstCtrl', ['$scope', function(scope) {
+    var todos = [
+        { text: "Learn Angular", done: 1365789942933 },
+        { text: "Speak about it" }
+    ];
+
+    scope.todos = todos;
+
+    scope.markDone = function(todo, checked) {
+        if(checked) {
+            todo.done = new Date().getTime();
+        } else {
+            delete todo.done;
+        }
+    }
+}]);
